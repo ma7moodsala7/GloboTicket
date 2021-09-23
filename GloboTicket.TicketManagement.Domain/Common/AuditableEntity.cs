49b522bf77c3f2b7e3b86efae71afc7d1811685b
Mahmoud Salah <ma7mood.sala7@gmail.com>
2021-09-23 17:45:04 +0200

    Creating the Domain project

---
+++ GloboTicket.TicketManagement.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GloboTicket.TicketManagement.Domain.Common
+{
+    /// <summary>
+    /// Base entity that contain a couple of base properties.
+    /// </summary>
+    public class AuditableEntity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
