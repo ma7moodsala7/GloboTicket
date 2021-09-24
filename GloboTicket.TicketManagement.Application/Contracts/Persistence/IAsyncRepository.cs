79b19f5b3e4b9551a7338593b3f344b88b98dec4
Mahmoud Salah <ma7mood.sala7@gmail.com>
2021-09-24 15:11:07 +0200

    Creating the application project

---
+++ GloboTicket.TicketManagement.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Contracts.Persistence
+{
+
+    /// <summary>
+    /// Base interface that is going to contain some generic methods on the repository
+    /// </summary>
+    // it's just a declaration of what an AsyncRepository should actually be capable of in a generic way.
+    public interface IAsyncRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
